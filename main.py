c071e8e6273f0503ba97feba66d52e997a090b74
микка <38471793+mikka1337@users.noreply.github.com>
2023-09-29 13:14:33 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,17 +9,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from colorama import init, Fore, Back, Style
 
-
-# Define the ASCII art
-ascii = f"""
-  ███╗░░██╗░██████╗████████╗██╗░█████╗░███╗░░░███╗
-  ████╗░██║██╔════╝╚══██╔══╝██║██╔══██╗████╗░████║
-  ██╔██╗██║╚█████╗░░░░██║░░░██║██║░░██║██╔████╔██║
-  ██║╚████║░╚═══██╗░░░██║░░░██║██║░░██║██║╚██╔╝██║
-  ██║░╚███║██████╔╝░░░██║░░░██║╚█████╔╝██║░╚═╝░██║
-  ╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░░░░╚═╝
-"""
-
 window_size = (800, 600)
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -49,7 +38,12 @@ def create_account():
     for _ in range(num_iterations):
         driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
         driver.get("https://www.roblox.com/")
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 30)
+        
+        cooki = driver.find_element(By.XPATH, "//button[@class='btn-secondary-lg cookie-btn btn-primary-md btn-min-width']")
+        cooki.click()
+        
+        time.sleep(1)
 
         dropdown = driver.find_element(By.CLASS_NAME, 'rbx-select')
         dropdown.click()
@@ -97,7 +91,7 @@ def create_account():
 
         def save_account_to_file(random_username, random_password):
             with open("Account.txt", "a") as file:
-                file.write(f"Username: {random_username}, Password: {random_password}\n")
+                file.write(f"{random_username}:{random_password}\n")
 
         time.sleep(10)
 
@@ -122,26 +116,12 @@ def Gender2(driver):
     Gender2 = driver.find_element(By.XPATH, "//*[@id='FemaleButton']")
     Gender2.click()
 
-def show_help():
-    help_text = "Channel 1: Enter the required quantity.\nChannel 2: Enter the starting name prefix. For example, NSTIOMHUB_ #output NSTIOMHUB_34DFBFR(If you want to generate random values without input)\nChannel 3: Enter the number of characters for the username. For example, 17 #output NSTIOMHUB_34DFBFR\n\n\nช่องที่ 1 ใส่จำนวนที่ต้องให้สร้าง\nช่องที่ 2 ใส่ชื่อที่ต้องให้ชึ้นต้น ตัวอย่าง NSTIOMHUB_ #output NSTIOMHUB_34DFBFR(ถ้าต้องการสุ่มไม่ต้องพิมไร)\nช่องที่ 3 คือจำนวนอักษรว่าจะให้ Username มีกี่ตัวอักษร ตัวอย่าง 17 #output NSTIOMHUB_34DFBFR"
-    help_window = tk.Toplevel(root)
-    help_window.title("Help")
-    help_label = tk.Label(help_window, text=help_text, justify="center")
-    help_label.pack()
-def discord():
-    url = "https://discord.gg/HQRkR6QPw4"
-    webbrowser.open(url)
 # Create the GUI
 root = tk.Tk()
-root.title("Roblox Account Creator")
-root.iconbitmap(r"img\icon.ico")
-root.geometry("500x400")
-ascii_label = tk.Label(root, text=ascii, justify="center", font=("Courier", 10))
-ascii_label.pack()
+root.title("EU R.A.G")
+root.geometry("240x180")
+root.resizable(False,False)
 
-# Add a label for your message
-credits_label = tk.Label(root, text="[!] Credits to DISCORD NSTIOM COMMUNITY.", justify="center")
-credits_label.pack()
 iterations_label = tk.Label(root, text="Enter the number of iterations:")
 iterations_label.pack()
 iterations_entry = tk.Entry(root)
@@ -159,10 +139,9 @@ namelength_entry.pack()
 
 create_button = tk.Button(root, text="Create Accounts", command=create_account)
 create_button.pack()
-discord_button = tk.Button(root, text="DISCORD", command=discord)
-discord_button.pack()
-help_button = tk.Button(root, text="Help", command=show_help)
-help_button.pack()
+
+edition_label = tk.Label(root, text="EU Edition fork")
+edition_label.pack()
 
 result_label = tk.Label(root, text="")
 result_label.pack()
